def1d374af092920ec9d515530050df1aa0403eb
kuzxnia <kacper.kuzniarski@gmail.com>
2018-04-13 21:21:54 +0200

    added endpoints to task#41

--- src/main/java/pl/pollub/cs/pentagoncafe/flare/service/user/UserServiceImpl.java
+++ src/main/java/pl/pollub/cs/pentagoncafe/flare/service/user/UserServiceImpl.java
@@ -3,20 +3,29 @@ package pl.pollub.cs.pentagoncafe.flare.service.user;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import pl.pollub.cs.pentagoncafe.flare.DTO.response.UserResponseDTO;
+import pl.pollub.cs.pentagoncafe.flare.domain.Event;
+import pl.pollub.cs.pentagoncafe.flare.domain.Participation;
 import pl.pollub.cs.pentagoncafe.flare.domain.User;
+import pl.pollub.cs.pentagoncafe.flare.domain.enums.Role;
 import pl.pollub.cs.pentagoncafe.flare.exception.ObjectNotFoundException;
 import pl.pollub.cs.pentagoncafe.flare.mapper.UserMapper;
+import pl.pollub.cs.pentagoncafe.flare.repository.event.EventRepository;
 import pl.pollub.cs.pentagoncafe.flare.repository.user.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private EventRepository eventRepository;
     private UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, EventRepository eventRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.eventRepository = eventRepository;
     }
 
     @Override
@@ -34,5 +43,43 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ObjectNotFoundException(User.class, "nick", nick));
 
         return userMapper.mapToResponseDTO(foundUser);
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsersAssignedToEvent(ObjectId EventId) {
+        Event event = eventRepository.findById(EventId)
+                .orElseThrow(() -> new ObjectNotFoundException(Event.class));
+
+        return event.getParticipation().stream()
+                .map(Participation::getUser)
+                .map(userMapper::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllAdmins() {
+        List<User> allUsers = userRepository.findAllByRoleIs(Role.ADMIN);
+
+        return allUsers.stream()
+                .map(userMapper::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+
+        return allUsers.stream()
+                .map(userMapper::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUnBannedUsers() {
+        List<User> foundUsers = userRepository.findAllByBannedIsFalse();
+
+        return foundUsers.stream()
+                .map(userMapper::mapToResponseDTO)
+                .collect(Collectors.toList());
     }
 }
